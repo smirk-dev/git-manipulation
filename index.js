ac05e185b0d814f1e9cea985709cd335f5032052
suryanshmishra <suryansh.mishra.sm6@gmail.com>
2025-07-09 13:00:08 +0530

    commit 1234567890abcdef1234567890abcdef12345678

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import moment from 'moment';
 import simpleGit from "simple-git";
 
 const path = './data.json';
-const date = moment().format();
+const date = moment().subtract(1, 'd').format();
 
 const data = {
   date: date,
 };
 
-jsonfile.writeFile(path, data);
+jsonfile.writeFile(path, data,()=>{simpleGit().add((path)).commit(date,{'--date': date}).push();});
 
-simpleGit().add((path)).commit(date,{'--date': date}).push();
+//simpleGit().add((path)).commit(date,{'--date': date}).push();
